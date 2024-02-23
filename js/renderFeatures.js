3bc0e434e967d9c0a94c60e92b314062f9d88331
John Asto <john.azt.dev@gmail.com>
2024-02-23 17:11:56 -0300

    add dinamy features

--- js/renderFeatures.js
+++ js/renderFeatures.js
@@ -1,31 +1,51 @@
 import { features } from './data.js';
 
-const featuresHTML = ({title, text, image, alt}) => {
-  return `
-    <div class="features__item">
-      <h3 class="features__item-title fs--lg">${title}</h3>
-      <p class="features__item-text">${text}</p>
-      <img class="features__item-image" src=${image} alt=${alt}>
-    </div>
-  `
+const createFeatureElement = ({title, text, image, alt}) => {
+  const featuresItem = document.createElement('div')
+  featuresItem.classList.add('features__item');
+
+  const titleEl = document.createElement('h3');
+  titleEl.textContent = title
+  titleEl.classList.add('features__item-title', 'fs--lg');
+
+  const textEl = document.createElement('p');
+  textEl.classList.add('features__item-text');
+  textEl.textContent = text;
+
+  const imageEl = document.createElement('img');
+  imageEl.classList.add('features__item-image');
+  imageEl.setAttribute('alt', alt);
+  imageEl.src = image;
+
+  featuresItem.appendChild(titleEl)
+  featuresItem.appendChild(textEl)
+  featuresItem.appendChild(imageEl)
+
+  return featuresItem
 }
 
 const renderFeaturesHTML = () => {
   const container = document.getElementById('features__grid');
   const fragment = document.createDocumentFragment();
 
-  const featuresElements = features.map(feature => {
-    const div = document.createElement('div')
-    div.innerHTML = featuresHTML(feature).trim();
-    return div.firstChild;
-  })
-
-  featuresElements.forEach(feature => {
-    fragment.appendChild(feature)
-  })
+  features.forEach(feature => {
+    const featuresItem = createFeatureElement(feature);
+    fragment.appendChild(featuresItem);
+  });
 
   container.appendChild(fragment)
 }
 
 
 renderFeaturesHTML()
+
+
+// const featuresHTML = ({title, text, image, alt}) => {
+//   return `
+//     <div class="features__item">
+//       <h3 class="features__item-title fs--lg">${title}</h3>
+//       <p class="features__item-text">${text}</p>
+//       <img class="features__item-image" src=${image} alt=${alt}>
+//     </div>
+//   `
+// }

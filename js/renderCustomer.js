b6b591ef9df04bd11dd141539fcc13ea5e972697
John Asto <john.azt.dev@gmail.com>
2024-02-23 15:14:16 -0300

    render html with js

--- js/renderCustomer.js
+++ js/renderCustomer.js
@@ -1,9 +1,7 @@
 import { columns } from "./data";
 
-
-const container = document.querySelector('.customers__grid');
+const container = document.getElementById('customers__grid');
 const fragment = document.createDocumentFragment();
-
 
 const renderColumns = () => {
   columns.forEach(column => {
@@ -14,6 +12,33 @@ const renderColumns = () => {
       const card = document.createElement('div')
       card.classList.add('customers__card', `customers__card-${customer.classname}`)
 
+      const text = document.createElement('p');
+      text.classList.add('customers__card-text');
+      text.textContent = customer.comment;
+
+      const userContainer = document.createElement('div');
+      userContainer.classList.add('customers__card-container-user');
+
+      const brand = document.createElement('div');
+      brand.classList.add('customers__card-brand', `customers__card-brand-${customer.classname}`);
+
+      const image = document.createElement('div');
+      image.classList.add('customers__card-image', `customers__card-image-${customer.classname}`);
+
+      const name = document.createElement('p');
+      name.classList.add('customers__card-name');
+      name.textContent = customer.name;
+
+      const job = document.createElement('span');
+      job.classList.add('customers__card-job');
+      job.textContent = customer.job;
+
+      userContainer.appendChild(brand)
+      userContainer.appendChild(image)
+      userContainer.appendChild(name)
+      userContainer.appendChild(job)
+      card.appendChild(text);
+      card.appendChild(userContainer);
       columnElement.appendChild(card)
     })
 
@@ -22,49 +47,4 @@ const renderColumns = () => {
 
   container.appendChild(fragment);
 }
-
-renderColumns()
-
-
-// columns.forEach(column => {
-//   const column = document.createElement('div');
-//   column.classList.add('customers__column', `customers__column-${customer.column}`);
-
-//   const card = document.createElement('div');
-//   card.classList.add('customers__card', `customers__card-${customer.classname}`);
-
-//   const text = document.createElement('p');
-//   text.classList.add('customers__card-text');
-//   text.textContent = customer.comment;
-
-//   const userContainer = document.createElement('div');
-//   userContainer.classList.add('customers__card-container-user');
-
-//   const brand = document.createElement('div');
-//   brand.classList.add('customers__card-brand', `customers__card-brand-${customer.classname}`);
-
-//   const image = document.createElement('div');
-//   image.classList.add('customers__card-image', `customers__card-image-${customer.classname}`);
-
-//   const name = document.createElement('p');
-//   name.classList.add('customers__card-name');
-//   name.textContent = customer.name;
-
-//   const job = document.createElement('span');
-//   job.classList.add('customers__card-job');
-//   job.textContent = customer.job;
-
-//   userContainer.appendChild(brand);
-//   userContainer.appendChild(image);
-//   userContainer.appendChild(name);
-//   userContainer.appendChild(job);
-
-//   card.appendChild(text);
-//   card.appendChild(userContainer);
-//   column.appendChild(card);
-//   fragment.appendChild(column);
-
-// });
-
-
-
+renderColumns();

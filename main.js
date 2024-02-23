f8671b23981d56b231137353a423394bcb06bf58
John Asto <john.azt.dev@gmail.com>
2024-02-23 17:57:02 -0300

    change file swiper

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+import './js/swiper_main.js'
 import './js/data.js'
 import './js/renderCustomer.js'
 import './js/render-why-rombo.js'
 import './js/renderFeatures.js'
 import './scss/style.scss'
-import './js/swiper_main.js'
+

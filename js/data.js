722f9765affa0fd5e35abe07851db783b5a44678
John Asto <john.azt.dev@gmail.com>
2024-02-23 14:54:37 -0300

    render elements in html

--- js/data.js
+++ js/data.js
@@ -1,20 +1,71 @@
-export const customers = [
+export const columns = [
   {
-    classname: 'mary',
-    comment: "Rombo's AI platform has been instrumental in driving data-driven success for our marketing strategies.With its advanced customers and tailor our campaigns for improved results!",
-    name: 'Mary Johnson',
-    job: 'CMO, InnovativeTech'
+    customers: [
+      {
+        classname: 'mary',
+        comment: "Rombo's AI platform has been instrumental in driving data-driven success for our marketing strategies.With its advanced customers and tailor our campaigns for improved results!",
+        name: 'Mary Johnson',
+        job: 'CMO, InnovativeTech'
+      },
+      {
+        classname: 'david',
+        comment: "Rombo's AI platform revolutionized our financial analysis. The actionable insights helped us make strategic decisions with confidence.",
+        name: 'David Smith',
+        job: 'CFO, LeadingEdge Enterprises'
+      },
+      {
+        classname: 'mary',
+        comment: "We are truly impressed with Rombo's AI integrations, especially how they have boosted our customer support and loyalty. The AI chatbot has been a game-changer for enhancing our user experience!",
+        name: 'Emily White',
+        job: 'CEO, Ecommerce Emporium'
+      },
+    ],
+    number: 'one'
   },
   {
-    classname: 'david',
-    comment: "Rombo's AI platform revolutionized our financial analysis. The actionable insights helped us make strategic decisions with confidence.",
-    name: 'David Smith',
-    job: 'CFO, LeadingEdge Enterprises'
+    customers: [
+      {
+        classname: 'mary',
+        comment: "Rombo's AI platform has been instrumental in driving data-driven success for our marketing strategies.With its advanced customers and tailor our campaigns for improved results!",
+        name: 'Mary Johnson',
+        job: 'CMO, InnovativeTech'
+      },
+      {
+        classname: 'david',
+        comment: "Rombo's AI platform revolutionized our financial analysis. The actionable insights helped us make strategic decisions with confidence.",
+        name: 'David Smith',
+        job: 'CFO, LeadingEdge Enterprises'
+      },
+      {
+        classname: 'mary',
+        comment: "We are truly impressed with Rombo's AI integrations, especially how they have boosted our customer support and loyalty. The AI chatbot has been a game-changer for enhancing our user experience!",
+        name: 'Emily White',
+        job: 'CEO, Ecommerce Emporium'
+      },
+    ],
+    number: 'two'
   },
   {
-    classname: 'mary',
-    comment: "We are truly impressed with Rombo's AI integrations, especially how they have boosted our customer support and loyalty. The AI chatbot has been a game-changer for enhancing our user experience!",
-    name: 'Emily White',
-    job: 'CEO, Ecommerce Emporium'
-  },
+    customers: [
+      {
+        classname: 'mary',
+        comment: "Rombo's AI platform has been instrumental in driving data-driven success for our marketing strategies.With its advanced customers and tailor our campaigns for improved results!",
+        name: 'Mary Johnson',
+        job: 'CMO, InnovativeTech'
+      },
+      {
+        classname: 'david',
+        comment: "Rombo's AI platform revolutionized our financial analysis. The actionable insights helped us make strategic decisions with confidence.",
+        name: 'David Smith',
+        job: 'CFO, LeadingEdge Enterprises'
+      },
+      {
+        classname: 'mary',
+        comment: "We are truly impressed with Rombo's AI integrations, especially how they have boosted our customer support and loyalty. The AI chatbot has been a game-changer for enhancing our user experience!",
+        name: 'Emily White',
+        job: 'CEO, Ecommerce Emporium'
+      },
+    ],
+    number: 'three'
+  }
 ]
--- js/renderCustomer.js
+++ js/renderCustomer.js
@@ -1,44 +1,70 @@
-import { customers } from "./data";
+import { columns } from "./data";
 
 
-    const container = document.querySelector('.customers__column-one');
-    const fragment = document.createDocumentFragment();
+const container = document.querySelector('.customers__grid');
+const fragment = document.createDocumentFragment();
 
-    customers.forEach(customer => {
-      const card = document.createElement('div');
-      card.classList.add('customers__card', `customers__card-${customer.classname}`);
 
-      const text = document.createElement('p');
-      text.classList.add('customers__card-text');
-      text.textContent = customer.comment;
+const renderColumns = () => {
+  columns.forEach(column => {
+    const columnElement = document.createElement('div');
+    columnElement.classList.add('customers__column', `customers__column-${column.number}`)
 
-      const userContainer = document.createElement('div');
-      userContainer.classList.add('customers__card-container-user');
+    column.customers.forEach(customer => {
+      const card = document.createElement('div')
+      card.classList.add('customers__card', `customers__card-${customer.classname}`)
 
-      const brand = document.createElement('div');
-      brand.classList.add('customers__card-brand', `customers__card-brand-${customer.classname}`);
+      columnElement.appendChild(card)
+    })
 
-      const image = document.createElement('div');
-      image.classList.add('customers__card-image', `customers__card-image-${customer.classname}`); // Replace with image logic
+    fragment.appendChild(columnElement)
+  })
 
-      const name = document.createElement('p');
-      name.classList.add('customers__card-name');
-      name.textContent = customer.name;
+  container.appendChild(fragment);
+}
 
-      const job = document.createElement('span');
-      job.classList.add('customers__card-job');
-      job.textContent = customer.job;
+renderColumns()
 
-      userContainer.appendChild(brand);
-      userContainer.appendChild(image); // Add image logic
-      userContainer.appendChild(name);
-      userContainer.appendChild(job);
 
-      card.appendChild(text);
-      card.appendChild(userContainer);
+// columns.forEach(column => {
+//   const column = document.createElement('div');
+//   column.classList.add('customers__column', `customers__column-${customer.column}`);
 
-      fragment.appendChild(card);
-    });
+//   const card = document.createElement('div');
+//   card.classList.add('customers__card', `customers__card-${customer.classname}`);
 
-    container.appendChild(fragment);
+//   const text = document.createElement('p');
+//   text.classList.add('customers__card-text');
+//   text.textContent = customer.comment;
+
+//   const userContainer = document.createElement('div');
+//   userContainer.classList.add('customers__card-container-user');
+
+//   const brand = document.createElement('div');
+//   brand.classList.add('customers__card-brand', `customers__card-brand-${customer.classname}`);
+
+//   const image = document.createElement('div');
+//   image.classList.add('customers__card-image', `customers__card-image-${customer.classname}`);
+
+//   const name = document.createElement('p');
+//   name.classList.add('customers__card-name');
+//   name.textContent = customer.name;
+
+//   const job = document.createElement('span');
+//   job.classList.add('customers__card-job');
+//   job.textContent = customer.job;
+
+//   userContainer.appendChild(brand);
+//   userContainer.appendChild(image);
+//   userContainer.appendChild(name);
+//   userContainer.appendChild(job);
+
+//   card.appendChild(text);
+//   card.appendChild(userContainer);
+//   column.appendChild(card);
+//   fragment.appendChild(column);
+
+// });
+
+
 

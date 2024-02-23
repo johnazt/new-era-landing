74cea51b3a2423bac0d5b83841b353593c60eeb2
John Asto <john.azt.dev@gmail.com>
2024-02-23 13:57:43 -0300

    create data for customers

---
+++ js/data.js
@@ -0,0 +1,20 @@
+export const customers = [
+  {
+    classname: 'mary',
+    comment: "Rombo's AI platform has been instrumental in driving data-driven success for our marketing strategies.With its advanced customers and tailor our campaigns for improved results!",
+    name: 'Mary Johnson',
+    job: 'CMO, InnovativeTech'
+  },
+  {
+    classname: 'david',
+    comment: "Rombo's AI platform revolutionized our financial analysis. The actionable insights helped us make strategic decisions with confidence.",
+    name: 'David Smith',
+    job: 'CFO, LeadingEdge Enterprises'
+  },
+  {
+    classname: 'mary',
+    comment: "We are truly impressed with Rombo's AI integrations, especially how they have boosted our customer support and loyalty. The AI chatbot has been a game-changer for enhancing our user experience!",
+    name: 'Emily White',
+    job: 'CEO, Ecommerce Emporium'
+  },
+]

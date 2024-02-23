99f6119b41d8cde234d14dccfed63837283105e5
John Asto <john.azt.dev@gmail.com>
2024-02-23 16:03:57 -0300

    add render why rombo

---
+++ js/render-why-rombo.js
@@ -0,0 +1,30 @@
+import { featuresRombo } from './data.js';
+
+
+const featuresRomboHTML = ({ image, title, text }) => {
+  return `<div class="why-rombo__item">
+  <img class="why-rombo__item-icon" src=${image} alt=${title}>
+  <h4 class="why-rombo__item-title">${title}</h4>
+  <p class="why-rombo__item-text">${text}</p>
+  </div>
+  `;
+}
+
+
+const renderHTMLFeatures = () => {
+  const container = document.getElementById('why-rombo__grid')
+  const fragment = document.createDocumentFragment();
+
+  const featuresElements = featuresRombo.map(feature => {
+    const div = document.createElement('div');
+    div.innerHTML = featuresRomboHTML(feature).trim();
+    return div.firstChild;
+  })
+   featuresElements.forEach(element => {
+    fragment.appendChild(element);
+  });
+
+  container.appendChild(fragment);
+}
+
+renderHTMLFeatures()

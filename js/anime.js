68f3a00b506ab1a3d37ac66d367235b6c5496d1b
John Asto <john.azt.dev@gmail.com>
2024-02-25 12:31:30 -0300

    try anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,17 +1,10 @@
-// anime({
-//   targets: '.levels__item',
-//   // translateY: 150,
-//   // backgroundColor: '#FFF',
-//   duration: 1000
-// });
-
 // Animation
-const animation = anime({
-  targets: '.levels__item',
-  translateX: '-300%',
-  direction: 'normal',
-  easing: 'easeInOutSine'
-});
+// const animation = anime({
+//   targets: '.levels__item',
+//   translateX: '-300%',
+//   direction: 'normal',
+//   easing: 'easeInOutSine'
+// });
 
 const triggerElementOne = document.querySelector(".levels__title");
 const triggerElementTwo = document.querySelector(".features__grid");
@@ -21,16 +14,15 @@ const animateOnScroll = function (div, speed = 500, offset = 0) {
   return (scrollPercent + offset) / speed;
 };
 
-// Scroll listener
-window.onscroll = function () {
-  animation.seek(animateOnScroll(triggerElementOne, 1000,400) * animation.duration);
-  animationTwo.seek(animateOnScroll(triggerElementTwo, 1000,400) * animation.duration);
-};
 
+// const animationTwo = anime({
+//   targets: '.features__grid',
+//   translateX: '-300%',
+//   direction: 'normal',
+//   easing: 'easeInOutSine'
+// });
 
-const animationTwo = anime({
-  targets: '.features__grid',
-  translateX: '-300%',
-  direction: 'normal',
-  easing: 'easeInOutSine'
-});
+// window.onscroll = function () {
+//   animation.seek(animateOnScroll(triggerElementOne, 1000,400) * animation.duration);
+//   animationTwo.seek(animateOnScroll(triggerElementTwo, 1000,400) * animation.duration);
+// };
